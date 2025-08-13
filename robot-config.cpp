b0f3721428a59539463ac67dd85d71691561ffaa
David <dvallejo2007@hotmail.com>
2025-08-13 11:05:50 -0500

    robot-config.cpp

--- robot-config.cpp
+++ robot-config.cpp
@@ -1,13 +1,20 @@
-#include "robot-config.h"
+#pragma once
+#include "vex.h"
+
+using namespace vex;
 
 // Motores lado izquierdo
-motor leftFront(PORT5, ratio18_1, false);
-motor leftMiddle(PORT3, ratio18_1, false);
-motor leftBack(PORT20, ratio18_1, false);
+extern motor leftFront;
+extern motor leftMiddle;
+extern motor leftBack;
 
 // Motores lado derecho
-motor rightFront(PORT1, ratio18_1, true);
-motor rightMiddle(PORT4, ratio18_1, true);
-motor rightBack(PORT11, ratio18_1, true);
+extern motor rightFront;
+extern motor rightMiddle;
+extern motor rightBack;
 
+// Sensor de inercia
+extern inertial InertialSensor;
 
+// Declaración de la función de inicialización
+void vexcodeInit(void);
