dd2ab3a439110b48fe3392b21a001563553c0c06
David <dvallejo2007@hotmail.com>
2025-09-12 08:48:28 -0500

    robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -1,17 +1,20 @@
 #pragma once
 #include "vex.h"
-
 using namespace vex;
 
-// Motores lado izquierdo
+// Motores individuales
 extern motor leftFront;
 extern motor leftMiddle;
+extern motor leftMiddle1;
 extern motor leftBack;
-
-// Motores lado derecho
 extern motor rightFront;
 extern motor rightMiddle;
+extern motor rightMiddle1;
 extern motor rightBack;
 
-// Sensor de inercia
+// Sensor
 extern inertial inertialSensor;
+
+// Grupos de motores (solo extern aquí)
+extern motor_group leftDrive;
+extern motor_group rightDrive;
